9c1821c1e236ee6a96e244ed6ae86aeff056799f
termistotel <termistotel@gmail.com>
2021-04-20 15:58:21 +0200

    Add bugfixes and aditional functionality to client

--- client/main.py
+++ client/main.py
@@ -1,11 +1,7 @@
 import kivy
-kivy.require('1.10.0') # replace with your current kivy version !
+kivy.require('2.0.0') # replace with your current kivy version !
 from kivy.app import App
-from pylib.mainbox import MainBox
-# from kivy.core.window import Window
 from kivy.config import Config
-from kivy.resources import resource_add_path, resource_find
-import sys
 
 class SvarogApp(App):
     def build(self):
@@ -13,10 +9,9 @@ class SvarogApp(App):
         return mainbox
 
 if __name__ == '__main__':
-    # Window.fullscreen = 'auto'
-    # Config.set('graphics', 'fullscreen', 'auto')
-    if hasattr(sys, '_MEIPASS'):
-        resource_add_path(os.path.join(sys._MEIPASS))
+    # if hasattr(sys, '_MEIPASS'):
+    #     resource_add_path(os.path.join(sys._MEIPASS))
     Config.set('graphics', 'window_state', 'maximized')
+    from pylib.mainbox import MainBox
     SvarogApp().run()
 
