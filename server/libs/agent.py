fe504f2f09d7161eab9feaddc548ffa9a786c8da
termistotel <termistotel@gmail.com>
2021-11-29 00:38:52 +0100

    Modify agent function

--- server/libs/agent.py
+++ server/libs/agent.py
@@ -1,36 +1,45 @@
-import tf
+import tensorflow as tf
+tf.compat.v1.disable_eager_execution()
 
 class LSTM(tf.keras.layers.Layer):
-    def __init__(self, output_shape, a_shape, activation = tf.nn.sigmoid):
+    def __init__(self, output_shape, activation=tf.nn.tanh):
         super(LSTM, self).__init__()
-        self.a_shape = a_shape
-        self.c_shape = a_shape
         self.gammao_dense_layers = []
         self.gammaf_dense_layers = []
         self.gammau_dense_layers = []
         self.c_dense_layers = []
-        self.output_dense_layers = []
         self.out_shape = output_shape
+        self.activation = activation
 
-        self.gammao_dense_layers.append(tf.keras.layers.Dense(self.c_shape[-1]))
-        self.gammao_dense_layers.append(tf.nn.sigmoid)
+        self.gammao_dense_layers.append(tf.keras.layers.Dense(self.out_shape,
+                                                              kernel_initializer='orthogonal',
+                                                              bias_initializer="zeros",
+                                                              activation=tf.nn.sigmoid))
+#         self.gammao_dense_layers.append(tf.nn.sigmoid)
 
-        self.gammaf_dense_layers.append(tf.keras.layers.Dense(self.c_shape[-1]))
-        self.gammaf_dense_layers.append(tf.nn.sigmoid)
+        self.gammaf_dense_layers.append(tf.keras.layers.Dense(self.out_shape, 
+                                                              kernel_initializer='orthogonal',
+                                                              activation=tf.nn.sigmoid,
+                                                              bias_initializer="ones"))
+#         self.gammaf_dense_layers.append(tf.nn.sigmoid)
 
-        self.gammau_dense_layers.append(tf.keras.layers.Dense(self.c_shape[-1]))
-        self.gammau_dense_layers.append(tf.nn.sigmoid)
+        self.gammau_dense_layers.append(tf.keras.layers.Dense(self.out_shape,
+                                                              kernel_initializer='orthogonal',
+                                                              bias_initializer="zeros",
+                                                              activation=tf.nn.sigmoid))
+#         self.gammau_dense_layers.append(tf.nn.sigmoid)
 
-        self.c_dense_layers.append(tf.keras.layers.Dense(self.c_shape[-1]))
-        self.c_dense_layers.append(tf.nn.tanh)
+        self.c_dense_layers.append(tf.keras.layers.Dense(self.out_shape,
+                                                         kernel_initializer='glorot_uniform',
+                                                         bias_initializer="zeros",
+                                                         activation=tf.nn.tanh))
+#         self.c_dense_layers.append(tf.nn.tanh)
 
-        self.output_dense_layers.append(tf.keras.layers.Dense(self.out_shape[-1]))
-        self.output_dense_layers.append(activation)
+        self.all_layers = self.gammao_dense_layers + self.gammaf_dense_layers + self.gammau_dense_layers + self.c_dense_layers
 
-        self.all_layers = self.gammao_dense_layers + self.gammaf_dense_layers + self.gammau_dense_layers + self.c_dense_layers + self.output_dense_layers
-
-    def call(self, inputs):
-        x, a, c = inputs
+    def call(self, inputs, initial_state = None):
+        x = inputs
+        a, c = initial_state
         tot_inputs = tf.concat([x, a], axis=-1)
 
         A = tot_inputs
@@ -54,13 +63,9 @@ class LSTM(tf.keras.layers.Layer):
         ctilde = A
 
         new_c = gammau*ctilde + gammaf*c
-        new_a = gammao*tf.nn.tanh(new_c)
+        new_a = gammao*self.activation(new_c)
 
-        A = new_a
-        for layer in self.output_dense_layers:
-            A = layer(A)
-        out = A
-        return out, new_a, new_c
+        return new_a, new_a, new_c
 
     def get_vars(self):
         weights = []
@@ -71,8 +76,8 @@ class LSTM(tf.keras.layers.Layer):
                 print(layer)
         return weights
 
-class Agent(tf.keras.Model):
-    def __init__(self, out_shape, c_shape, hln, f):
+class Agent():
+    def __init__(self, out_shape, c_shape, hln, f, out_activation = tf.nn.sigmoid):
         super(Agent, self).__init__()
         self.all_layers = []
         self.c_shape = c_shape
@@ -81,25 +86,20 @@ class Agent(tf.keras.Model):
         self.f = f
 
         for _ in range(self.hln):
-            self.all_layers.append(LSTM([f], c_shape, activation = tf.nn.tanh))
-        self.all_layers.append(LSTM(out_shape, c_shape, activation = tf.nn.sigmoid))
+            layer = LSTM(f)
+            self.all_layers.append(layer)
+        self.output_layer = tf.keras.layers.Dense(out_shape, activation=out_activation)
 
-    def call(self, inputs):
-        x = inputs[0]
-        ass = inputs[1:2+hln]
-        css = inputs[2+hln:]
-#         print(len(ass), len(css))
-        new_as, new_cs = [], []
+    def __call__(self, inputs, states):
+        new_states = []
 
-        A = x
-#         for layer, a, c in zip(self.all_layers, ass, css):
+        A = inputs
         for i in range(len(self.all_layers)):
-            layer, a, c = self.all_layers[i], ass[i], css[i]
-            A, new_a, new_c = layer([A, a, c])
-            new_as.append(new_a)
-            new_cs.append(new_c)
-        out = A
-        return [out, *new_as, *new_cs]
+            layer, s = self.all_layers[i], states[i]
+            A, new_a, new_c = layer(A, initial_state = s)
+            new_states.append((new_a, new_c))
+        out = self.output_layer(A)
+        return [out, new_states]
 
     def get_vars(self):
         weights = []
